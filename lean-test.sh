07268766b0f38a6fdb899e12ac8655ebea30ffe1
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-04-24 22:44:03 -0300

    adding a simple script

---
+++ lean-test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./prod/bin/cvc5 $@ &> tmpResult
+tail -n +2 tmpResult > tmpResult.lean
+rm tmpResult
+cp tmpResult.lean ~/cvc/signatures/lean4/Cdclt/examples/
+cd ~/cvc/signatures/lean4/
+leanpkg build
+cd -
