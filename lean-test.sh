2a160f9ad2613c36bea0f492f94e4abb2a0f079e
Haniel Barbosa <hanielbbarbosa@gmail.com>
2021-06-13 19:28:54 -0300

    updating test script

--- lean-test.sh
+++ lean-test.sh
@@ -3,7 +3,9 @@
 ./prod/bin/cvc5 $@ &> tmpResult
 tail -n +2 tmpResult > tmpResult.lean
 rm tmpResult
-cp tmpResult.lean ~/cvc/signatures/lean4/Cdclt/examples/
-cd ~/cvc/signatures/lean4/
-leanpkg print-paths Cdclt.examples.tmpResult
-cd -
+~/cvc/signatures/lean4/build/bin/Cdclt tmpResult.lean
+# cp tmpResult.lean ~/cvc/signatures/lean4/Cdclt/examples/
+# cd ~/cvc/signatures/lean4/
+# leanpkg print-paths Cdclt.examples.tmpResult
+# rm build/Cdclt/examples/tmpResult.olean
+# cd -
