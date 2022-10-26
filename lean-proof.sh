6751c64ffd210490b820fa5b6a3b13396dc819ef
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-10-26 14:10:36 -0300

    new script

--- lean-proof.sh
+++ lean-proof.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-~/cvc/wt-leanPrinter/prod/bin/cvc5 $@ | tail -n +2
+tfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
+outfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
+
+./prod/bin/cvc5 --dump-proofs --proof-format=lean --proof-granularity=theory-rewrite --dag-thresh=0 $@ | tail -n +2 > $tfile
+
+cd ~/lean-smt
+lean --load-dynlib=/home/hbarbosa/lean-smt/build/lib/libSmt.so $tfile > $outfile
+cd - > /dev/null
+if ! grep -q "error" $outfile; then
+  echo "SUCCESS"
+else
+  echo "ERROR-LEAN"
+fi
