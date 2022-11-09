b6a8699eaa921c3596b47296de64bad84aad5526
Haniel Barbosa <hanielbbarbosa@gmail.com>
2022-11-09 10:56:35 -0300

    update script to printing proof as s-expression

--- lean-proof.sh
+++ lean-proof.sh
@@ -3,7 +3,11 @@
 tfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
 outfile="$(mktemp /tmp/foo.XXXXXXXXX)" || exit 1
 
-./prod/bin/cvc5 --dump-proofs --proof-format=lean --proof-granularity=theory-rewrite --dag-thresh=0 $@ | tail -n +2 > $tfile
+# produce proof, remove first like (which is "unsat"), then remove
+# last line (which is closing the s-expression string), then remove
+# the s-expression/string prefix
+
+./prod/bin/cvc5 --dump-proofs --proof-format=lean --proof-granularity=theory-rewrite --dag-thresh=0 $@ | tail -n +2 | head -n -1 | sed 's/(proof \"//' > $tfile
 
 cd ~/lean-smt
 lean --load-dynlib=/home/hbarbosa/lean-smt/build/lib/libSmt.so $tfile > $outfile
